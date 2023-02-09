1f1c5371f58c1ffe8fa1fc702413f8cf324317b7
Santosh177 <117721403+Santosh177@users.noreply.github.com>
2023-02-09 22:08:39 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,14 +3,14 @@ import './../styles/App.css';
 
 const App = (props) => {
 const [isDisabled, seIsDisabled] = useState(false)
-const handleClick = ()=>{
+const handleClick = () =>{
   seIsDisabled(true);
 };
   return (
     <div className="App" id="main">
       // Do not alter the main div
     <p id="para" disabled={isDisabled}>"Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy"</p>
-    <button id="click" onClick=(handleClick}>click me </button>
+    <button id="click" onClick={handleClick}>click me </button>
     </div>
   );
 }
